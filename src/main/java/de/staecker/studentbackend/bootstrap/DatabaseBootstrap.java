100c5a0f9cea55752fadd5fe3ff61a42cf330eae
Andreas Staecker <andreas.staecker@t-online.de>
2018-10-03 20:24:20 +0200

    switch to master 10 students

--- src/main/java/de/staecker/studentbackend/bootstrap/DatabaseBootstrap.java
+++ src/main/java/de/staecker/studentbackend/bootstrap/DatabaseBootstrap.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import de.staecker.studentbackend.generator.StudentFactory;
-import de.staecker.studentbackend.repository.StudentJpaRepository;
 
 public class DatabaseBootstrap implements InitializingBean{
 	
--- src/main/java/de/staecker/studentbackend/generator/StudentFactory.java
+++ src/main/java/de/staecker/studentbackend/generator/StudentFactory.java
@@ -1,8 +1,5 @@
 package de.staecker.studentbackend.generator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
