f53a839b557134d5bb40a07d7a0760c6ca887de8
Andreas Staecker <andreas.staecker@t-online.de>
2018-10-20 11:32:55 +0200

    Controller erweitert

--- src/main/java/de/staecker/studentbackend/controller/StudentController.java
+++ src/main/java/de/staecker/studentbackend/controller/StudentController.java
@@ -3,8 +3,11 @@ package de.staecker.studentbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +21,42 @@ public class StudentController {
 	@Autowired
 	StudentJpaRepository studentJpaRepository;
 	
-	@GetMapping(value="/all")
+	/**
+	 * Alle Studenten ausgeben.
+	 * @return Student.
+	 */
+	@GetMapping(value="/")
 	public List <Student> findAll(){
 		return studentJpaRepository.findAll();
 	}
 	
+	/**
+	 * Einene bestimmten Studenten ausgeben.
+	 * @param id
+	 * @return Student
+	 */
 	@GetMapping(value="/{id}")
 	public Student findById(@PathVariable final Long id ) {
 	return studentJpaRepository.findById(id).get();
 		
+	}
+	
+	/**
+	 * Einene Studenten speichern.
+	 * @param student (Student, welcher im Body als JSON übergeben wird)
+	 * @return Neuer gespeicherter Student mit ID.
+	 */
+	@PostMapping(value="/save")
+	public Student save(@RequestBody final Student user) {
+		return studentJpaRepository.save(user);
+	}
+	
+	/**
+	 * Einen Studenten löschen.
+	 * @param id ID des zu löschenden Studenten.
+	 */
+	@DeleteMapping(value="/delete/{id}")
+	public void delete(@PathVariable final Long id) {
+		studentJpaRepository.deleteById(id);
 	}
 }
